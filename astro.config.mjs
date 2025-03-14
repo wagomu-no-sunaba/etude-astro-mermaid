b4c3e57dee0bbd2dbb8e11729e725762978c2b85
staticWagomU <to6wa17@gmail.com>
2025-03-14 23:31:12 +0900

    feat: rehype-mermaidを適用

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid', 'js'],
+    },
+    rehypePlugins: [rehypeMermaid],
+  }
+});
